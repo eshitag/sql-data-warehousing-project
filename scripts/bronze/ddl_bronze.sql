88917f4d200434a7786fb95d0c71b966e94d1045
Eshita Gupta <48042648+eshitag@users.noreply.github.com>
2026-01-21 15:09:09 -0330

    Create bronze tables for CRM and ERP data
    This script creates multiple tables in the bronze schema for customer, product, sales, and ERP information.

---
+++ scripts/bronze/ddl_bronze.sql
@@ -0,0 +1,58 @@
+-- DDL script: creates bronze tables
+USE Datawarehouse
+
+-- checks for table if it already exists
+IF OBJECT_ID('bronze.crm_cust_info', 'U') IS NOT NULL
+	DROP TABLE bronze.crm_cust_info;
+
+CREATE TABLE bronze.crm_cust_info(
+cst_id INT,
+cst_key NVARCHAR(50),
+cst_firstname NVARCHAR(50),
+cst_lastname NVARCHAR(50),
+cst_material_status NVARCHAR(50),
+cst_gndr NVARCHAR(50),
+cst_create_date DATE
+);
+
+CREATE TABLE bronze.crm_prod_info(
+prd_id INT,
+prd_key NVARCHAR(50),
+prd_nm NVARCHAR(50),
+prd_cost FLOAT,
+prd_line NVARCHAR(50),
+prd_start_dt DATE,
+prd_end_dt DATE
+)
+
+-- DROP TABLE bronze.crm_sales_details;
+
+CREATE TABLE bronze.crm_sales_details(
+sls_ord_num NVARCHAR(50),
+sls_prd_key NVARCHAR(50),
+sls_cust_id INT,
+sls_order_dt NVARCHAR(50),
+sls_ship_dt NVARCHAR(50),
+sls_due_dt NVARCHAR(50),
+sls_sales INT,
+sls_quantity INT,
+sls_price FLOAT,
+);
+
+CREATE TABLE bronze.erp_cust_az12(
+cid NVARCHAR(50),
+bdate DATE,
+gen NVARCHAR(50)
+);
+
+CREATE TABLE bronze.erp_loc_a101(
+cid NVARCHAR(50),
+cntry NVARCHAR(50)
+);
+
+CREATE TABLE bronze.erp_px_cat_g1v2(
+id NVARCHAR(50),
+cat NVARCHAR(50),
+subcat NVARCHAR(50),
+maintenance NVARCHAR(50)
+);
